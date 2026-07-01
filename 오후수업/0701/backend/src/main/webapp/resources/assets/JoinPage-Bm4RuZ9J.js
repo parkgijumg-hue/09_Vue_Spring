5e9c8aa8a3a0b821c0e8471437e8b03dcca8a1c7
박기정 <parkgijumg@gmail.com>
2026-07-01 16:17:33 +0900

    0701

---
+++ 오후수업/0701/backend/src/main/webapp/resources/assets/JoinPage-Bm4RuZ9J.js
@@ -0,0 +1,1 @@
+import{_ as e,c,e as n}from"./index-Bp7PQe2L.js";const o={};function r(t,a){return n(),c("h1",null,"JoinPage")}const _=e(o,[["render",r]]);export{_ as default};
---
+++ 오후수업/0701/frontend/src/components/layouts/menu/LogoutMenuItem.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+const logout = (e) => {
+  // 실제 로그아웃 로직 구현 예정
+  router.push('/');
+};
+</script>
+
+<template>
+  <a href="#" class="nav-link" @click.prevent="logout">
+    <i class="fa-solid fa-right-from-bracket"></i>
+    로그아웃
+  </a>
+</template>
